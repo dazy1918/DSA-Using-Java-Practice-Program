f22d2c43aea3094d87cdd9c98aef8d50cb71606b
Dazy Baliyan <baliyandazy22@gmail.com>
2026-02-19 12:59:38 +0530

    Add recursive function to print numbers in decreasing order
    Implement a recursive method to print numbers in decreasing order from n to 1.

---
+++ Recursion Program/DecreasingOrder.java
@@ -0,0 +1,17 @@
+// Print numbers from n to 1(Decreasing Order)
+
+public class Recursion1 {
+    public static void printDec(int n) {
+        if (n == 1) {
+            System.out.println(n);
+        }
+        System.out.println(n + " ");
+        printDec(n - 1);
+    }
+
+    public static void main(String[] args) {
+        int n = 10;
+        printDec(n);
+
+    }
+}
